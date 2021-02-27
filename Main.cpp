2c67bbcb11b8e6f7fe4ef91ae5513d86b90dca1f
Fernando Ocaña <ferrubocana@gmail.com>
2021-02-26 23:36:02 -0600

    Se ha añadido un pequeño menú inicial

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    //Declaramos las principales variables que utilizaremos
+    int carretasTotales, cajasTotales;
+
+    cout << "¡BIENVENIDO AL SIMULADOR DE SUPERMERCADO 3000!" << endl;
+    cout << "Ingrese el número de carretas con las que el supermercado contará: ";
+    cin >> carretasTotales;
+    cout << "Ingrese el número de cajas con las que el supermercado contará: ";
+    cin >> cajasTotales;
+
+    cout << "Carretas: " << carretasTotales << "    Cajas: " << cajasTotales;
+
+    return 0;
+}
